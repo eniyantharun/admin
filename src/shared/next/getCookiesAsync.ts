c6a9c8750d1a712fd2d862a372f2653977ba668f
tharuneniyan <tharuneniyan@gmail.com>
2025-07-19 00:02:36 +0530

    vv

--- src/shared/next/getCookiesAsync.ts
+++
@@ -1,7 +0,0 @@
-import { cookies } from 'next/headers'
-import type { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
-
-// better typing and name
-export async function getCookiesAsync(): Promise<ReadonlyRequestCookies> {
-  return await cookies()
-}
